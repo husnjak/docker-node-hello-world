03788863c272e9a8ffa45aba18d6498e9baa72c6
husnjak <husnjak@student.chalmers.se>
2019-05-20 11:04:58 +0200

    fixed broken test

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ describe('request', () => {
     it('shit works', (done) => {
         request('http://localhost:4000')
             .then(function (htmlString) {
-                assert.equal(htmlString, 'Hello World\n');
+                assert.equal(htmlString, 'Captain Awesome was here!\n');
                 done();
             })
             .catch(function (err) {
